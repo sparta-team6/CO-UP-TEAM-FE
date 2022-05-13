c350d111cd5341b18e0b520c670d957f23e91492
김지호 <crsn1111@gmail.com>
2022-05-13 15:51:55 +0900

    [refactor] 기능 수정

--- src/pages/Tool/Document/DocumentList.tsx
+++ src/pages/Tool/Document/DocumentList.tsx
@@ -6,30 +6,27 @@ import MyProjectList from "../../../layout/MyProjectList";
 
 const Document = () => {
   const { data } = useGetFolders();
-  console.log(data?.data.length);
   return (
     <>
-      {data?.data.length === 0 ? (
-        <div className="w-full  h-[calc(100vh-3rem)] bg-slate-300 flex absolute bottom-0">
-          <div className="flex fixed top-0 left-0 mt-12 h-full sm:hidden">
-            <MyProjectList />
-            <DocumentList />
-          </div>
-          <div className="w-full h-full flex ml-[336px] p-4 md:justify-center sm:p-2 sm:m-0">
-            <ViewDoc />
-          </div>
+      <div className="w-full  h-[calc(100vh-3rem)] bg-slate-300 flex absolute bottom-0">
+        <div
+          className={`flex fixed top-0 left-0 mt-12 h-full ${
+            data?.data.length === 0 ? "sm:hidden" : "sm:w-full"
+          }`}
+        >
+          <MyProjectList />
+          <DocumentList />
         </div>
-      ) : (
-        <div className="w-full  h-[calc(100vh-3rem)] bg-slate-300 flex absolute bottom-0">
-          <div className="flex fixed top-0 left-0 mt-12 h-full sm:w-full">
-            <MyProjectList />
-            <DocumentList />
-          </div>
-          <div className="w-[calc(100%-41rem)] h-full flex ml-[336px] p-4 md:w-[calc(100%-21rem)] md:justify-center sm:hidden sm:p-2 sm:m-0">
-            <ViewDoc />
-          </div>
+        <div
+          className={`${
+            data?.data.length === 0
+              ? "w-full h-full flex ml-[336px] p-4 md:justify-center sm:p-2 sm:m-0"
+              : " w-[calc(100%-41rem)] h-full flex ml-[336px] p-4 md:w-[calc(100%-21rem)] md:justify-center sm:hidden sm:p-2 sm:m-0"
+          }`}
+        >
+          <ViewDoc />
         </div>
-      )}
+      </div>
       <Chat />
     </>
   );
