7f12f27be2cb8426751d4fa86f899b02ef1827fe
김경래 <mosbisu@gmail.com>
2022-05-14 00:13:44 +0900

    [docs] 보드페이지 슬라이드

--- src/pages/Tool/Board.tsx
+++ src/pages/Tool/Board.tsx
@@ -1,14 +1,37 @@
+import { useEffect } from "react";
+import SlidingPanel from "react-sliding-side-panel";
+import { useRecoilState } from "recoil";
 import BoardList from "../../layout/BoardList";
 import Chat from "../../layout/Chat";
 import MyProjectList from "../../layout/MyProjectList";
+import { HandleOpen } from "../../recoil/Atoms";
 
 const Board = () => {
+  const [open, setOpen] = useRecoilState(HandleOpen);
+
+  useEffect(() => {
+    setOpen(false);
+  }, []);
   return (
-    <div className="w-full h-[calc(100vh-3rem)] bg-slate-300 flex absolute bottom-0">
-      <MyProjectList />
-      <BoardList />
+    <>
+      <div className="w-full h-[calc(100vh-3rem)] bg-slate-300 flex absolute bottom-0">
+        <div className="flex sticky h-full">
+          <div className="sm:hidden">
+            <MyProjectList />
+          </div>
+          <div className="hidden sm:block sm:w-screen">
+            <SlidingPanel type={"left"} isOpen={open} size={100}>
+              <MyProjectList />
+            </SlidingPanel>
+            <BoardList />
+          </div>
+          <div className="w-[calc(100vw-23rem)] md:w-[calc(100vw-3rem)] sm:hidden">
+            <BoardList />
+          </div>
+        </div>
+      </div>
       <Chat />
-    </div>
+    </>
   );
 };
 
