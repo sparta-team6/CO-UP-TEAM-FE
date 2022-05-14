2521003a3bca66a67e9cf3275e81b33d213ba96b
김지호 <crsn1111@gmail.com>
2022-05-15 01:44:25 +0900

    [feat] 보드 관련 API 추가

--- src/api/BoardQuery.ts
+++ src/api/BoardQuery.ts
@@ -1,8 +1,21 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { instance } from "../servers/axios";
+
+interface Board {
+  pjId: string;
+  title: string;
+  position: number;
+}
 
 export const useGetBoard = (pjId: string) => {
   return useQuery("getBoard", () => {
-    return instance.get(`/api/buckets/?pjId=${pjId}`);
+    return instance.get(`api/buckets/?pjId=${pjId}`);
+  });
+};
+
+export const usePostBoard = () => {
+  return useMutation(async (post: Board) => {
+    await instance.post("api/buckets/", post);
+    console.log(post);
   });
 };
