6299803ffdefcd4ca4855b20290ab5353f81fe26
김지호 <crsn1111@gmail.com>
2022-05-16 22:02:42 +0900

    [docs] 문서 수정

--- src/layout/BoardList.tsx
+++ src/layout/BoardList.tsx
@@ -1,31 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 import { Board, useGetBoard } from "../api/BoardQuery";
 import Bucket from "../Components/ToolBoard/Bucket";
-import { BoardState, ProjectKey } from "../recoil/Atoms";
+import { ProjectKey } from "../recoil/Atoms";
 
 const BoardList = () => {
   const { pjId } = useRecoilValue(ProjectKey);
   const { data: board } = useGetBoard(String(pjId));
-  const [test, setTest] = useRecoilState(BoardState);
+  const [test, setTest] = useState(board?.data);
   useEffect(() => {
     setTest(board?.data);
   }, [board]);
+  console.log(test);
   const onDragEnd = ({ destination, source }: DropResult) => {
     if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       // same board movement.
       setTest((allBoards: any) => {
         const boardCopy = [...allBoards[Number(source.droppableId)].cards];
+        const ThisKbbId = allBoards[Number(source.droppableId)].kbbId;
         const taskObj = boardCopy[source.index];
         boardCopy.splice(Number(source.index), 1);
         boardCopy.splice(destination?.index, 0, taskObj);
+        console.log(ThisKbbId);
         console.log(boardCopy);
-        return {
+        /* boardCopy로 변한 cards 배열을 어케하는지 */
+        const test111 = {
           ...allBoards,
           [Number(source.droppableId)]: boardCopy,
         };
+        return test111;
       });
     }
     /* 다른 버킷으로 이동 */
@@ -33,15 +38,20 @@ const BoardList = () => {
       // cross board movement
       setTest((allBoards: any) => {
         const sourceBoard = [...allBoards[Number(source.droppableId)].cards];
+        const StartKbbId = allBoards[Number(source.droppableId)].kbbId;
         const taskObj = sourceBoard[source.index];
         const destinationBoard = [...allBoards[Number(destination.droppableId)].cards];
+        const LastKbbId = allBoards[Number(destination.droppableId)].kbbId;
         sourceBoard.splice(source.index, 1);
         destinationBoard.splice(destination?.index, 0, taskObj);
-        return {
+        console.log(StartKbbId, LastKbbId);
+        console.log(sourceBoard, destinationBoard);
+        const test112 = {
           ...allBoards,
           [source.droppableId]: sourceBoard,
           [destination.droppableId]: destinationBoard,
         };
+        return test112;
       });
     }
   };
