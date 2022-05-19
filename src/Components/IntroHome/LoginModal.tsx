7ed054cfe1d233289203e6279b3f80cda9e755d0
김지호 <crsn1111@gmail.com>
2022-05-20 00:45:27 +0900

    [style]테스트 입장 버튼 수정

--- src/Components/IntroHome/LoginModal.tsx
+++ src/Components/IntroHome/LoginModal.tsx
@@ -10,13 +10,14 @@ import { useMyInfo } from "../../api/UserQuery";
 import { useSetRecoilState } from "recoil";
 import { MyProfile } from "../../recoil/MyProfile";
 
+/* h-[480px] 입니다 */
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 784,
-  height: 480,
+  height: 520,
   bgcolor: "background.paper",
   boxShadow: 12,
   p: 4,
@@ -44,7 +45,7 @@ export default function LoginModal() {
       </motion.div>
       <motion.button
         whileHover={{ scale: 1.05 }}
-        className="w-[336px] h-[96px] rounded-xl text-[32px] mt-[70px] font-extrabold sm:hidden bg-3 text-white"
+        className="w-[280px] h-[80px] rounded-xl text-[32px] mt-[70px] font-extrabold sm:hidden bg-3 text-white"
         onClick={handleOpen}
       >
         CO-UP 시작하기
@@ -55,7 +56,8 @@ export default function LoginModal() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style} className="w-[500px] h-[370px] rounded-xl">
+        {/* h-[370px]입니다 */}
+        <Box sx={style} className="rounded-xl">
           <div className="flex flex-col items-center">
             <span className="font-bold text-4xl p-8">{`로그인을 해주세요:)`}</span>
             <KakaoLogin />
@@ -63,10 +65,10 @@ export default function LoginModal() {
             <NaverLogin />
             <Link
               onClick={() => SetUser(user)}
-              className="w-[314px] h-[54px] text-white bg-black mt-9 rounded-lg flex justify-center items-center"
+              className="w-[314px] h-[54px] text-white bg-4 mt-9 rounded-lg flex justify-center items-center"
               to="/projectList"
             >
-              <span>프론트 테스트 접속</span>
+              <span>테스트 입장</span>
             </Link>
           </div>
         </Box>
