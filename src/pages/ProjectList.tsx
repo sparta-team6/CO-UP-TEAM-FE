11b680fdf977a2d29425a67b236668bc21b6972b
김지호 <crsn1111@gmail.com>
2022-05-20 09:29:10 +0900

    [docs] 문서 수정

--- src/pages/ProjectList.tsx
+++ src/pages/ProjectList.tsx
@@ -11,10 +11,9 @@ const ProjectList = () => {
   const { data } = useMyInfo();
   const user = data?.data;
   console.log(data?.data);
-  console.log("hi");
   useEffect(() => {
     SetUser(user);
-  }, []);
+  }, [data]);
   return (
     <div className="w-full h-[calc(100vh-3rem)] bg-[#f0f3f7] flex flex-col items-center justify-center absolute bottom-0">
       <div className="w-full h-full flex flex-col items-center mt-20">
