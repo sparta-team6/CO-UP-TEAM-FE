06d11aedb0bf3191703d2ae23b1d436b23fc5015
김지호 <crsn1111@gmail.com>
2022-05-24 14:42:22 +0900

    [style] 스타일 수정

--- src/layout/TeamList.tsx
+++ src/layout/TeamList.tsx
@@ -5,8 +5,8 @@ import Member from "../Components/ToolMain/Member";
 const TeamList = () => {
   return (
     <React.Fragment>
-      <div className="fixed w-72 h-full flex flex-col sm:h-[calc(100vh-9rem)] left-20 top-16 bg-[#fff] dark:bg-gray-800">
-        <div className="w-72 h-72 flex flex-col items-center">
+      <div className="w-72 h-full flex flex-col sm:h-screen bg-[#fff] dark:bg-gray-800">
+        <div className="w-72 h-72 flex flex-col items-center sm:mt-16">
           <Chart />
           <div className="w-[248px] h-[1px] bg-gray-200 -mt-2" />
         </div>
