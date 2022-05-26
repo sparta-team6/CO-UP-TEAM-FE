1b2f1e58591bb0175b6430ecd1b1b1c2a98842e8
김경래 <mosbisu@gmail.com>
2022-05-26 16:00:06 +0900

    [feat] sockjs, stompjs

--- src/pages/Tool/TestRoom.tsx
+++ src/pages/Tool/TestRoom.tsx
@@ -3,57 +3,58 @@ import { useRecoilValue } from "recoil";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
 import { ProjectKey } from "../../recoil/RoomID";
+import { ChatPresenter } from "./TestRoomPresenter";
 
-export interface message {
-  username: string;
-  content: string;
+export interface content {
+  senderLoginId: string;
+  message: string;
+  pjId: string;
 }
 
 /* 기본 api url 주소 */
-/* const sockJS = new SockJS("http://localhost:8080/webSocket");
-const stompClient: Stomp.Client = Stomp.over(sockJS); */
+const sockJS = new SockJS("https://api.cooperate-up.com/ws");
+const stompClient: Stomp.Client = Stomp.over(sockJS);
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-/* stompClient.debug = () => {}; */
+stompClient.debug = () => {};
 
 const TestRoom = () => {
-  const [contents, setContents] = React.useState<message[]>([]);
-  const [username, setUsername] = React.useState("");
-  const [message, setMessage] = React.useState("");
+  const [messages, setMessages] = React.useState<content[]>([]);
+  const [senderLoginId, setSenderLoginId] = React.useState("");
+  const [content, setContent] = React.useState("");
   const { pjId } = useRecoilValue(ProjectKey);
 
   useEffect(() => {
-    /*  stompClient.connect({}, () => {
-      백엔드에서 설정한 pjId별 채팅 room 
-      stompClient.subscribe(`/${pjId}`, (data) => {
-        const newMessage: message = JSON.parse(data.body) as message;
+    stompClient.connect({}, (frame) => {
+      console.log(frame);
+      // 백엔드에서 설정한 pjId별 채팅 room
+      stompClient.subscribe(`sub/chatting/${pjId}`, (data) => {
+        const newMessage: content = JSON.parse(data.body) as content;
         addMessage(newMessage);
       });
-    }); */
-  }, [contents]);
+    });
+  }, [messages]);
 
-  const handleEnter = (username: string, content: string) => {
-    const newMessage: message = { username, content };
+  const handleEnter = (senderLoginId: string, message: string, pjId: string) => {
+    const newMessage: content = { senderLoginId, message, pjId };
     /* 백엔드에서 요청 post 보내는 api 주소 */
-    /*  stompClient.send("/hello", {}, JSON.stringify(newMessage)); */
-    setMessage("");
+    stompClient.send("/pub/chatting/project", {}, JSON.stringify(newMessage));
+    console.log("hi");
+    setContent("");
   };
 
-  const addMessage = (message: message) => {
-    setContents((prev) => [...prev, message]);
+  const addMessage = (content: content) => {
+    setMessages((prev) => [...prev, content]);
   };
   return (
-    <div className="w-full h-screen bg-3 flex justify-center items-center">
-      <div className="w-96 h-96 bg-4">
-        hi
-        {/* <ChatPresenter
-          contents={contents}
-          handleEnter={handleEnter}
-          message={message}
-          setMessage={setMessage}
-          username={username}
-          setUsername={setUsername}
-        /> */}
-      </div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <ChatPresenter
+        messages={messages}
+        handleEnter={handleEnter}
+        content={content}
+        setContent={setContent}
+        senderLoginId={senderLoginId}
+        setSenderLoginId={setSenderLoginId}
+      />
     </div>
   );
 };
