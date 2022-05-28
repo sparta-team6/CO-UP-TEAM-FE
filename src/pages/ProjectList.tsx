161c00dfcb33a38ab96f04faf1ba4bdf2ddbbc2e
김경래 <mosbisu@gmail.com>
2022-05-29 01:38:37 +0900

    [test] 튜토리얼 테스트

--- src/pages/ProjectList.tsx
+++ src/pages/ProjectList.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { useMyInfo } from "../api/UserQuery";
 import ProjectData from "../Components/ProjectList/ProjectData";
 import ProjectMake from "../Components/ProjectList/ProjectMake";
 import ProjectOpen from "../Components/ProjectList/ProjectOpen";
+import JoyrideContainer from "../Components/Tutorial/JoyrideContainer";
+import { projectListSteps } from "../Components/Tutorial/Steps";
 import { MyProfile } from "../recoil/MyProfile";
 import { getAccessTokenFromCookie, getFreshTokenFromCookie } from "../servers/Cookie";
 
@@ -29,20 +31,22 @@ const ProjectList = () => {
   // }, [navigate]);
 
   return (
-    <div className="w-full h-auto bg-[#f0f3f7] flex flex-col items-center justify-center">
-      <div className="w-full h-full flex flex-col items-center pt-36 sm:pt-32">
-        <div className=" w-[1188px] flex justify-between items-center mb-8 md:w-[90%]">
-          <span className="text-4xl leading-[50px] sm:text-3xl">프로젝트 리스트</span>
-          <ProjectOpen />
-        </div>
-        <div className="w-[1188px] h-full flex flex-wrap justify-start sm:justify-center md:w-[90%] md:h-auto">
-          <ProjectData />
-          <div className="w-[288px] h-[320px] rounded-lg border-[1px] border-solid border-gray-300 flex justify-center items-center sm:w-full mb-[20px]">
-            <ProjectMake />
+    <>
+      <div className="pl1 w-full h-auto bg-[#f0f3f7] flex flex-col items-center justify-center relative">
+        <div className="w-full h-full flex flex-col items-center pt-36 sm:pt-32">
+          <div className=" w-[1188px] flex justify-between items-center mb-8 md:w-[90%]">
+            <span className="text-4xl leading-[50px] sm:text-3xl">프로젝트 리스트</span>
+            <ProjectOpen />
+          </div>
+          <div className="w-[1188px] h-full flex flex-wrap justify-start sm:justify-center md:w-[90%] md:h-auto">
+            <ProjectData />
+            <div className="w-[288px] h-[320px] rounded-lg border-[1px] border-solid border-gray-300 flex justify-center items-center sm:w-full mb-[20px]">
+              <ProjectMake />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
