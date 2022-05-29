eabb1feeef37ac5c2b1441d51b6ad981a907761f
김경래 <mosbisu@gmail.com>
2022-05-29 23:13:03 +0900

    [feat] 소셜로그인 아이콘 추가

--- src/elements/IntroHome/GoogleLogin.tsx
+++ src/elements/IntroHome/GoogleLogin.tsx
@@ -1,14 +1,16 @@
 import { GOOGLE_AUTH_URL } from "../../servers/OAuth";
+import Google from "../../images/Login/GoogleLogin.jpg";
 
 const GoogleLogin = () => {
   const onClick = () => {
     window.location.href = GOOGLE_AUTH_URL;
   };
   return (
-    <div onClick={onClick}>
-      <button className="w-[314px] h-[54px] text-white bg-[#5F99FF] mt-[24px] rounded-lg">
+    <div onClick={onClick} className="relative">
+      <button className="w-[314px] h-[54px] text-black bg-white mt-[24px] rounded-lg">
         구글 로그인
       </button>
+      <img width={40} height={40} src={Google} alt="" className="top-[31px] left-10 absolute" />
     </div>
   );
 };
