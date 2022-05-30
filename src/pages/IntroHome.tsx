b6aed60776bff2880a0dfa0d773dc56c927da7c3
김지호 <crsn1111@gmail.com>
2022-05-31 02:05:08 +0900

    [style] 모바일 스타일 개선

--- src/pages/IntroHome.tsx
+++ src/pages/IntroHome.tsx
@@ -20,7 +20,7 @@ const IntroHome = () => {
       <Helmet>
         <title>CO-UP</title>
       </Helmet>
-      <div className="w-full h-screen sm:h-auto cu:bg-[url('../images/Intro/IntroBg.png')] sm:bg-[#ffffff]  dark:bg-8 flex flex-col items-center">
+      <div className="w-full h-screen sm:h-auto cu:bg-[url('../images/Intro/IntroBg.png')] cu:dark:bg-[url('../images/Intro/Dark/IntroBgDark.png')] sm:bg-[#ffffff]  dark:bg-8 flex flex-col items-center">
         <div className="w-[1192px] sm:w-full mb-9 flex justify-end sm:justify-center sm:min-h-screen rounded-sm">
           <LoginModal />
         </div>
