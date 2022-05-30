436af20769d308f717f8c09d649f5446a7a606ae
김경래 <mosbisu@gmail.com>
2022-05-30 23:55:05 +0900

    [feat] 내보내기 버튼 추가

--- src/Components/ToolMain/Member.tsx
+++ src/Components/ToolMain/Member.tsx
@@ -6,19 +6,27 @@ import { useGetProjectUser } from "../../api/UserQuery";
 import { useKickRoom } from "../../api/ProjectQuery";
 import Swal from "sweetalert2";
 import { queryClient } from "../..";
+import { useState } from "react";
 
 const Member = () => {
   const { pjId, projectRole } = useRecoilValue(ProjectKey);
   const { data } = useGetProjectUser(pjId);
   const TeamUsers = data?.data.slice(1);
   const { mutateAsync: KickUser } = useKickRoom(pjId);
+  const [kickOpen, setKickOpen] = useState(false);
+  const handleKick = () => {
+    setKickOpen(!kickOpen);
+  };
   const onClick = (loginId: string, nickname: string) => {
     Swal.fire({
-      title: "추방",
-      text: `${nickname}님을 추방시키겠습니까?`,
+      title: `${nickname}님을 추방시키겠습니까?`,
+      text: "추방시키면 다시 프로젝트에 참가할 수 없습니다.",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonText: "넵!",
-      cancelButtonText: "취소!",
+      confirmButtonText: "추방하자",
+      cancelButtonText: "취소하자",
+      confirmButtonColor: "#5F99FF",
+      cancelButtonColor: "#D7DCE5",
     }).then((result) => {
       if (result.value) {
         KickUser(loginId).then((res) => {
@@ -30,9 +38,18 @@ const Member = () => {
   const projectAdmin = data?.data[0];
   return (
     <div className="tutorial-main3 w-full h-full">
-      <div className="flex items-center mt-[28px] sm:mt-[15px]">
-        <SvgUser />
-        <span className="text-lg font-bold ml-1 dark:text-white">팀원</span>
+      <div className="flex justify-between items-center mt-[28px] sm:mt-[15px]">
+        <div className="flex items-center">
+          <SvgUser />
+          <span className="text-lg font-bold ml-1 dark:text-white">팀원</span>
+        </div>
+        {projectRole === "ADMIN" && (
+          <div>
+            <button onClick={handleKick} className="w-[62px] h-[24px] bg-[#E7EBF2] rounded text-xs">
+              내보내기
+            </button>
+          </div>
+        )}
       </div>
       <div className="group w-full mt-[20px] relative flex items-center space-x-2">
         <img className="absolute -top-1 left-6" src={imgCrown} alt="" />
@@ -77,19 +94,30 @@ const Member = () => {
       {TeamUsers?.map((teamUser, index) => {
         return (
           <div key={index} className="group w-full mt-[12px] relative flex items-center space-x-2">
-            <img
-              className="rounded-full m-0"
-              width={36}
-              height={36}
-              src={teamUser.profileImage}
-              alt=""
-            />
-            <span className="font-semibold dark:text-white">{teamUser.nickname}</span>
-            {projectRole === "ADMIN" && (
-              <button onClick={() => onClick(String(teamUser.loginId), String(teamUser.nickname))}>
-                추방
-              </button>
-            )}
+            <div className="w-full flex justify-between">
+              <div className="flex items-center space-x-2">
+                <img
+                  className="rounded-full m-0"
+                  width={36}
+                  height={36}
+                  src={teamUser.profileImage}
+                  alt=""
+                />
+                <span className="font-semibold dark:text-white mt-[2px] pr-[10px]">
+                  {teamUser.nickname}
+                </span>
+              </div>
+              <div>
+                {kickOpen && (
+                  <button
+                    className="flex justify-center items-center text-2xl w-[24px] h-[24px] bg-[#DE4A06] rounded-full text-white mr-[38px]"
+                    onClick={() => onClick(String(teamUser.loginId), String(teamUser.nickname))}
+                  >
+                    -
+                  </button>
+                )}
+              </div>
+            </div>
             <div className="sm:left-[-30%] sm:top-[-350px] hidden w-[334px] min-h-[120px] sm:z-50 bg-5 dark:bg-8 border-[#E7EBF2] dark:border-[#666666] border-[1px] border-solid group-hover:flex sm:group-focus:block absolute right-[-315px] top-[-80px] rounded-lg shadow-md">
               <div className="w-full h-full px-[20px] py-[13px] flex flex-col">
                 <div className="w-full h-full flex">
