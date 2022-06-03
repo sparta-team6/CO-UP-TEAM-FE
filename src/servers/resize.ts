3681263dc9b28bc2d1d24c71b48af0aa0b80e8f0
김지호 <crsn1111@gmail.com>
2022-06-03 21:41:25 +0900

    [chore] 커스텀 훅

--- src/servers/resize.ts
+++ src/servers/resize.ts
@@ -1,5 +1,6 @@
 import FileResizer from "react-image-file-resizer";
 
+// 이미지 리사이징 CustomHook
 export const resizeFile = (file: Blob, x: number, y: number, type: string) =>
   new Promise((resolve) => {
     FileResizer.imageFileResizer(
